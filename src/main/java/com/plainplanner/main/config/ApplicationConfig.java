1e8610e5ec89a7ef25116c95ec5addc5ba1c24cd
Nuno Rodrigues <nrodrigues0213@gmail.com>
2020-09-03 18:58:05 -0400

    Add resource mapping

--- src/main/java/com/plainplanner/main/config/ApplicationConfig.java
+++ src/main/java/com/plainplanner/main/config/ApplicationConfig.java
@@ -6,14 +6,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.plainplanner.services.CustomUserDetailsService;
 
 @Configuration
-public class ApplicationConfig {
+public class ApplicationConfig implements WebMvcConfigurer {
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
 }
